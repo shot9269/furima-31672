c9d1d3928d88b6897b0ec8516152839a96f924ca
shot9269 <110fe603@gmail.com>
2020-10-30 17:02:08 +0900

    テーブル作成

--- db/migrate/20201030060438_devise_create_users.rb
+++ db/migrate/20201030060438_devise_create_users.rb
@@ -4,6 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, default: "" 
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name_kana,    null: false, default: ""
+      t.string :last_name_kana,     null: false, default: ""
+      t.date   :birthday,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
