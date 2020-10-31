d20cc586ed5c4e641916d7f5788beda6bdf5f495
shot9269 <110fe603@gmail.com>
2020-10-31 14:12:35 +0900

    rubocop実行済み

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,:password_confirmation,:first_name,:last_name,:first_name_kana,:last_name_kana,:birthday, presence: true
-  validates :email, uniqueness: true, format: {with: /@/}
-  validates :password,length: {minimum: 6},format: {with: /\A[a-zA-Z0-9]+\z/},confirmation: true
-  validates :first_name,:last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name_kana,:last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :nickname, :password_confirmation, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+  validates :email, uniqueness: true, format: { with: /@/ }
+  validates :password, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }, confirmation: true
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                {Faker::Name.name }
-    password                {Faker::Internet.password(min_length: 6)}
-    password_confirmation   {password}
-    birthday                {Faker::Date.birthday}
-    first_name              {"山田"}
-    last_name               {"太郎"}
-    first_name_kana         {"ヤマダ"}
-    last_name_kana          {"タロウ"}
-    email                   {Faker::Internet.free_email}
+    nickname                { Faker::Name.name }
+    password                { Faker::Internet.password(min_length: 6) }
+    password_confirmation   { password }
+    birthday                { Faker::Date.birthday }
+    first_name              { '山田' }
+    last_name               { '太郎' }
+    first_name_kana         { 'ヤマダ' }
+    last_name_kana          { 'タロウ' }
+    email                   { Faker::Internet.free_email }
   end
 end
