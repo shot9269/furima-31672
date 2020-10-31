086a335c60eb9b107c4e072c630a98b23b75a4ed
shot9269 <110fe603@gmail.com>
2020-10-31 14:43:22 +0900

    validation with_optionに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,31 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :password_confirmation, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
-  validates :email, uniqueness: true, format: { with: /@/ }
-  validates :password, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }, confirmation: true
-  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  # validates :nickname, :password_confirmation, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+  # validates :email, uniqueness: true, format: { with: /@/ }
+  # validates :password, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }, confirmation: true
+  # validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  # validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+  with_options presence: true do
+    validates :nickname
+    validates :password_confirmation
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :first_name_kana
+    validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+    validates :password, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }, confirmation: true
+  end
+
 end
