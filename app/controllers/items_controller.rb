d027ffa7ef7f168c1f5e0b7d9d073b24a81ebbf4
shot9269 <110fe603@gmail.com>
2020-11-02 18:04:26 +0900

    バリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name,:content,:price,:image,:category_id,:condition_id,:fee_id,:area_id,:send_id).merge(user_id: params[:user_id])
+    params.require(:item).permit(:name,:content,:price,:category_id,:condition_id,:fee_id,:area_id,:send_id,:image,).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,21 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :price, numericality: {greater_than: 300, less_than:9999999}
+
   with_options presence: true do
+    validates :image
     validates :name
     validates :content
     validates :price
+    validates :user_id
+    validates :condition_id
+    validates :fee_id
+    validates :area_id
+    validates :send_id
+  end
+
+  with_options numericality: {other_than:1} do
     validates :user_id
     validates :condition_id
     validates :fee_id
